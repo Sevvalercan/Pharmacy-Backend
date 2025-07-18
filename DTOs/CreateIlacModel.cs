6eb089e547eb02de00152fd95c482dc9dbea64f9
Şevval <88663122+Sevvalercan@users.noreply.github.com>
2025-07-18 22:14:57 +0300

    IlacFiltreResponse eklendi

--- DTOs/CreateIlacModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pharmacy_Backend.DTOs
-{
-    public class CreateIlacModel
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Barcode { get; set; }
-    }
-}
